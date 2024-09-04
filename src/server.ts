102da2f3ca29eac6e4ca6ea3c2a284ad14d4eaa7
Terje Karlsen <terjeofnorway@gmail.com>
2024-09-04 20:40:31 +0200

    Add local server functionality

--- src/server.ts
+++ src/server.ts
@@ -9,21 +9,23 @@ const port = 3000;
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
-  res.sendStatus(404);
+    res.sendStatus(404);
 });
 
 app.post('/lighthouse-report', (req, res) => {
-  const { version } = req.body;
+    const { version } = req.body;
 
-  if (!version) {
-    res.sendStatus(400);
-    return;
-  }
+    if (!version) {
+        res.sendStatus(400);
+        return;
+    }
 
-  sendToSlack({ version });
-  res.sendStatus(200);
+    sendToSlack({ version });
+    res.sendStatus(200);
 });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
+
+export default app;
